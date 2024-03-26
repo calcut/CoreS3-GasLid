97c4858d11befd94811f251b7647cf4200674f19
Calum Cuthill <calum.cuthill@gmail.com>
2024-03-26 16:33:07 +0000

    moving ui stuff

--- src/ui/gui_helpers.h
+++ src/ui/gui_helpers.h
@@ -3,14 +3,7 @@
 
 #include <lvgl.h>
 #include <math.h>
-
-#ifdef GASLID
-#include <ui_gaslid/ui.h>
-#endif
-#ifdef SEPTICSENSOR
-#include <ui_septicsensor/ui.h>
-#endif
-
+#include "ui.h"
 #include "gui_populate_widgets.h"
 #include "rtos_tasks.h"
 
